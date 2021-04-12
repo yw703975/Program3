2ad872b8027bcba92554d9f7ec6d4ff2fe965a16
yw703975 <yw703975@southeast.edu>
2021-04-12 08:26:12 -0500

    Modified Myfitness.sql

--- MyFitness.sql
+++ MyFitness.sql
@@ -2,27 +2,26 @@
 DROP DATABASE IF EXISTS MyFitness;
 CREATE DATABASE MyFitness;
 USE MyFitness;  -- MySQL command
-CREATE TABLE userProfile(
-  userID       INT(11)        NOT NULL   AUTO_INCREMENT,
+CREATE TABLE Users(
+  userID       INT(11)        NOT NULL   AUTO_INCREMENT  PRIMARY KEY,
   firstName     VARCHAR(30)   NOT NULL,
   lastName      VARCHAR(30)   NOT NULL,
   userName      VARCHAR(30)   NOT NULL,
   passWord      VARCHAR(80)   NOT NULL,
   email          VARCHAR(30)  NOT NULL,
   sex           VARCHAR(10)    NULL,
-  birthDay       date          NULL,
+  birthDay       DATE          NULL,
   height         VARCHAR(30)   NOT NULL,
-  userPhoto    VARCHAR(30)    NULL,
-constraint pkuserID  PRIMARY KEY (userID)
+  userPhoto    VARCHAR(30)    NULL
 );
 
 CREATE TABLE userData(
     userID       INT(11)        NOT NULL,
-    beginWorkout  SMALLDATETIME    NULL,
-    endWorkout    SMALLDATETIME    NULL,
+    beginWorkout  DATE            NULL,
+    endWorkout    DATE             NULL,
     stepNumber    INT(6)           NULL,
     weight        INT(6)           NULL,
-constraint fkuserID foreign key(userID) references userProfile(userID)
+constraint fkuserID foreign key(userID) references Users(userID)
 );
 
 
